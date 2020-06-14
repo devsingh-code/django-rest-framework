541fd2d28ac6f5a7686c68c5a0393128a39aa0ec
Deovrat Singh <devsingh6n@gmail.com>
2020-06-14 16:13:13 +0530

    Added class based views, mixins, and generic views with permission concept

--- django_rest/urls.py
+++ django_rest/urls.py
@@ -15,12 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from posts.views import  PostView, post_list,post_detail
+from posts.views import (
+    PostView,
+    post_list,post_detail,PostMixinListView,
+    PostListView,
+    PostDetailView,
+    PostDestroyView
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/posts/', PostListView.as_view(),name='post-list'),
+    path('api/posts/<int:pk>/', PostDetailView.as_view(),name='post-detail'),
+    path('api/posts/<int:pk>/delete/', PostDestroyView.as_view(),name='post-destroy'),
+    #path('api/posts/', PostMixinListView.as_view(),name='post-list'),
     #path('api/posts/', PostView.as_view(),name='post-list'),
-    path('api/post-list/',post_list,name='post-list'),
-    path('api/posts/<int:pk>/',post_detail,name='post-detail'),
+    #path('api/posts/<int:pk>/',PostView.as_view(),name='post-detail')
+    #path('api/post-list/',post_list,name='post-list'),
+    #path('api/posts/<int:pk>/',post_detail,name='post-detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import mixins
+from rest_framework import generics
 from posts.serializers import PostSerializer
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED,HTTP_400_BAD_REQUEST,HTTP_204_NO_CONTENT
 from rest_framework.parsers import JSONParser
 from .models import Post
+from .permissions import IsOwnerPermission
 # Create your views here.
 from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -17,6 +21,30 @@ class PostView(APIView):
         serializer = PostSerializer(queryset, many=True)
         #if serializer.is_valid():
         return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer=PostSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status = HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request,pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request,pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
 
 @csrf_exempt
 def post_list(request):
@@ -45,7 +73,7 @@ def post_detail(request, pk):
         serializer = PostSerializer(post)
         return JsonResponse(serializer.data)
 
-    if request.method == 'PUT':
+    elif request.method == 'PUT':
         data = JSONParser.parse(request)
         serializer = PostSerializer(post, data=data)
         if serializer.is_valid():
@@ -53,6 +81,40 @@ def post_detail(request, pk):
             return JsonResponse(serializer.data,)
         return JsonResponse(serializer.errors, status=400)
 
-    if request.method == 'DELETE':
+    elif request.method == 'DELETE':
         post.delete()
         return HttpResponse(status=204)
+
+
+
+
+
+class PostMixinListView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    '''self.list() is a special method that comes with ListModelMixin'''
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+'''This directly act as get method and returns the posts in list form '''
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes=(IsAuthenticated,)
+
+class PostDetailView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes=(IsAuthenticated,IsOwnerPermission)
+
+class PostDestroyView(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
